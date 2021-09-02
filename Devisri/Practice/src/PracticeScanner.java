46dfbd6d193d378c112e7ae520f751e28e035908
siri1592 <devisri1592@gmail.com>
2021-09-02 21:09:28 +0530

    commit added

--- Devisri/Practice/src/PracticeScanner.java
+++ Devisri/Practice/src/PracticeScanner.java
@@ -1,35 +1,36 @@
-import java.math.BigInteger;
+
+//import java.math.BigInteger;
 import java.util.Scanner;
 
 public class PracticeScanner {
     public static void main(String[] args) {
         System.out.println("******Bank Deposit/Withdrawl Details********");
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter Your Account Number: ");
-        long aNum=scanner.nextLong();
+        long aNum = scanner.nextLong();
         scanner.nextLine();
         System.out.println("Enter Your Name: ");
-        String name=scanner.nextLine();
+        String name = scanner.nextLine();
         System.out.println("Your Withdrawl amount is: ");
-        double withdrawl=scanner.nextDouble();
-        System.out.println("Checking: "+scanner.hasNextDouble());
+        double withdrawl = scanner.nextDouble();
+        System.out.println("Checking: " + scanner.hasNextDouble());
         System.out.println("Your deposit is: ");
-        double deposit=scanner.nextDouble();
+        double deposit = scanner.nextDouble();
         System.out.println("Did You re-check the entered details Y/N ");
-        char check=scanner.next().charAt(0);
-        System.out.println("Checking: "+scanner.hasNext());
+        char check = scanner.next().charAt(0);
+        System.out.println("Checking: " + scanner.hasNext());
         scanner.nextLine();
-        //System.out.println("Please submit: ");
+        // System.out.println("Please submit: ");
 
-        boolean isSubmit=false;
+        boolean isSubmit = false;
 
-        System.out.println("Your Account Number: "+aNum);
-        System.out.println("Your Name: "+name);
-        System.out.println("Your Withdrawl amount is: "+withdrawl);
-        System.out.println("Your deposit is: "+deposit);
-        System.out.println("Did You re-check the entered details Y/N "+check);
+        System.out.println("Your Account Number: " + aNum);
+        System.out.println("Your Name: " + name);
+        System.out.println("Your Withdrawl amount is: " + withdrawl);
+        System.out.println("Your deposit is: " + deposit);
+        System.out.println("Did You re-check the entered details Y/N " + check);
         while (!isSubmit) {
-            String s=scanner.nextLine();
+            String s = scanner.nextLine();
             isSubmit = s.equals("submit");
         }
         scanner.close();
