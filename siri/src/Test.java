3e92a6bdc342abc9971054666e574c906f36b0b1
siriy9999 <sirimurali412@gmail.com>
2021-09-06 23:11:58 +0530

    Assinment

---
+++ siri/src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    int num;
+    String name;
+    void Print(){
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        Test oTest=new Test();
+     System.out.println(oTest.num+" "+oTest.name);
+     oTest.Print();
+
+        
+        
+    }
+    
+}
