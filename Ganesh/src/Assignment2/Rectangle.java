360327bb7c0e133ce4a7a7ada7d635827f26222e
Ganesh T <ganeshkrishna.tu@gmail.com>
2021-09-07 10:53:35 +1000

    assignment files

--- Ganesh/src/Assignment2/Rectangle.java
+++ Ganesh/src/Assignment2/Rectangle.java
@@ -1,13 +1,19 @@
 package Assignment2;
 
 public class Rectangle {
+    double area ;
     public Rectangle(double length,double breath){
-        double area = (length*breath)/2;
-        System.out.println("Area of Rectangle : " + area);
+        area = (length*breath)/2;
+       // System.out.println("Area of Rectangle : " + area);
+    }
+    void display(){
+        System.out.println("Area of Rectangle : "+area);
     }
     public static void main(String[] args){
         Rectangle areaobj = new Rectangle(4,5);
         Rectangle areaobj1 = new Rectangle(5,8);
+        areaobj.display();
+        areaobj1.display();
     }
     
 }
--- Ganesh/src/Assignment2/RectangleArea.java
+++ Ganesh/src/Assignment2/RectangleArea.java
@@ -1,5 +1,26 @@
 package Assignment2;
 
+import java.util.Scanner;
+
 public class RectangleArea {
-    
-}
+    double length,breath,area;
+    public RectangleArea(double l,double b){
+        Scanner sc =new Scanner(System.in);
+        l = sc.nextDouble();
+        b = sc.nextDouble();
+        this.length = l;
+        this.breath = b;
+        
+        sc.close();
+
+    }
+    public double returnArea(){
+        area = length*breath;
+        return area;
+    }
+    public static void main(String[] args){
+        RectangleArea RA = new RectangleArea(0,0);
+        RA.returnArea();
+        System.out.println("Area of Rectangle : "+RA.area);
+    }
+    }
