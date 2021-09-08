e7f3cf2966d8ff242eaabeef24d52ec12c89f046
siriy9999 <sirimurali412@gmail.com>
2021-09-08 18:28:24 +0530

    pract

---
+++ siri/practice/palindrom.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class palindrom {
+public static void main(String[] args) {
+    Scanner oScanner=new Scanner(system.in);
+    System.out.println("enter number");
+    oScanner.nextInt();  
+}
+    
+}
