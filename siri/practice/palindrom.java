3b320d69f11543567c7bcf18a2bd0930c8e6469f
siriy9999 <sirimurali412@gmail.com>
2021-09-08 23:32:01 +0530

    fibon

--- siri/practice/palindrom.java
+++ siri/practice/palindrom.java
@@ -4,7 +4,15 @@ public class palindrom {
 public static void main(String[] args) {
     Scanner oScanner=new Scanner(system.in);
     System.out.println("enter number");
-    oScanner.nextInt();  
+    oScanner.nextInt(); 
+    int n=0,r, sum=0;
+    temp=n;
+    while(temp>0){
+        r=n%10;
+        sum=(sum*10)+10;
+        
+    }
+
 }
     
 }
