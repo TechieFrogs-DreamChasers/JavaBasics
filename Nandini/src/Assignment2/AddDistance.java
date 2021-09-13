f8bda817b4b88057b1e4e81bf02a1d8e2370df11
Nandini-2986 <nandu.garige@gmail.com>
2021-09-13 14:49:34 -0500

    commit

--- Nandini/src/Assignment2/AddDistance.java
+++ Nandini/src/Assignment2/AddDistance.java
@@ -2,14 +2,41 @@ package Assignment2;
 
 public class AddDistance {
     
-    int distance1;
-    int distance2;
+    double inch;
+    double feet;
 
-    public static void main(String[] args) {
-        
+    public AddDistance(double inch,double feet){
+
+        this.inch = inch;
+        this.feet = feet;
+
     }
 
-    void calculateDistance(){
+    public static void main(String[] args) {
+
+        AddDistance d1 = new AddDistance(5, 6);
+        AddDistance d2 = new AddDistance(10, 12);
         
+        AddDistance result = calculateDistance(d1,d2);
+        
+        result.showDistance();
+        //d1.showDistance();
+        //d2.showDistance();
+    }
+
+    static AddDistance calculateDistance(AddDistance d1,AddDistance d2){
+
+        AddDistance d = new AddDistance(0,0);
+        d.inch = d1.inch + d2.inch;
+        d.feet = d1.feet + d2.feet + (d.inch/12);
+        d.inch = d.inch%12;
+        System.out.println(d.inch + d.feet);
+        return d;
+    }
+
+    void showDistance(){
+
+       System.out.println("incehes:  " + inch);
+       System.out.println("feet: " + feet);
     }
 }
