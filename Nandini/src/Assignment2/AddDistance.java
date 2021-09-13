380625693d7ff24bbef3b0704f71281b056c844f
Nandini-2986 <nandu.garige@gmail.com>
2021-09-13 12:30:23 -0500

    commit

--- Nandini/src/Assignment2/AddDistance.java
+++ Nandini/src/Assignment2/AddDistance.java
@@ -2,4 +2,14 @@ package Assignment2;
 
 public class AddDistance {
     
+    int distance1;
+    int distance2;
+
+    public static void main(String[] args) {
+        
+    }
+
+    void calculateDistance(){
+        
+    }
 }
