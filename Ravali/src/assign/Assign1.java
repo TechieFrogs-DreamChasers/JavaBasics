3c5f58fcb31bb3ba0f187108340bd487f0eaadca
desaniravalireddy@gmail.com <desaniravalireddy@gmail.com>
2021-09-15 02:56:43 +0530

    assignment

---
+++ Ravali/src/assign/Assign1.java
@@ -0,0 +1,25 @@
+package assign;
+class parent{
+    void display(){
+        System.out.println("This is a parent class");
+    }
+
+}
+class child extends parent{
+    void displayout(){
+        System.out.println("This is a child class ");
+    }
+}
+
+public class Assign1 {
+    public static void main (String[]args){
+        parent obj = new parent();
+        child obj1 = new child();
+   obj.display();
+obj1.displayout();
+obj1.display();
+    }
+    
+
+    
+}
