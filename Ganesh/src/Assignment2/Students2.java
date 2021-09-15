2dfa38370696d992221f5743e651624945a7cd37
Ganesh T <ganeshkrishna.tu@gmail.com>
2021-09-16 08:32:48 +1000

    commit

--- Ganesh/src/Assignment2/Students2.java
+++ Ganesh/src/Assignment2/Students2.java
@@ -16,8 +16,7 @@ public class Students2 {
    public static void main(String[] args){
        Students2 obj1 = new Students2("John",2,995895846,"68D-WallsStreet");
        Students2 obj2 = new Students2("Sam",3,566859425,"28B-Wallstreet");
-       System.out.println("Student 1 :" + obj1.name+" "+obj1.roll_no+" "+ obj1.Phone_No+" "+ obj1.address);
-      
+       System.out.println("Student 1 :" + obj1.name+" "+obj1.roll_no+" "+ obj1.Phone_No+" "+ obj1.address);      
        System.out.println("Student 2 :" + obj2.name+" "+ obj2.roll_no+" "+ obj2.Phone_No+" "+ obj2.address);
      
    }
---
+++ Ganesh/src/Assignment3/Manager.java
@@ -0,0 +1,9 @@
+package Assignment3;
+
+public class Manager extends Members {
+    String Department = "Programming";
+    public static void main(String[] args){
+        Manager Obj1 = new Manager();
+        Obj1.printSalary();
+    }
+}
