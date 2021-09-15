c5ae97a02db72a8388fcb89fcba77951b8bfa722
Nandini-2986 <nandu.garige@gmail.com>
2021-09-15 11:12:01 -0500

    commit

---
+++ Nandini/src/Assignment3/Exercise3/Employee.java
@@ -0,0 +1,6 @@
+package Assignment3.Exercise3;
+
+public class Employee extends Member{
+    
+    String specialization;
+}
---
+++ Nandini/src/Assignment3/Exercise3/Member.java
@@ -0,0 +1,31 @@
+package Assignment3.Exercise3;
+
+public class Member {
+
+        String name;
+        int age;
+        String phoneNumber;
+        String address;
+        double salary;
+
+        /*Member(String name,int age,String phoneNumber,String address,double salary){
+            this.name = name;
+            this.age = age;
+            this.phoneNumber = phoneNumber;
+            this.address = address;
+            this.salary = salary;
+        }*/
+
+        void printSalary(){
+            System.out.println("Salary of the Members: " + salary);
+        }
+
+        void printDetails(){
+            System.out.println("Name: "+ name);
+            System.out.println("Age: " + age);
+            System.out.println("PhoneNumber: " + phoneNumber);
+            System.out.println("Address: " + address);
+            System.out.println("Salary: " + salary);
+
+        }
+}
