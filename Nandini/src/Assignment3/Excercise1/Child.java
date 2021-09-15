87d5f350b8e22bd98c1d4f94e7a5bf2a35ac71d0
Nandini-2986 <nandu.garige@gmail.com>
2021-09-14 23:12:21 -0500

    commit

--- Nandini/src/Assignment3/Excercise1/Child.java
+++ Nandini/src/Assignment3/Excercise1/Child.java
@@ -2,18 +2,12 @@ package Assignment3.Excercise1;
 
 public class Child extends Parent {
     
-    void prints(){
-        super.prints();
+    void printing(){
+        //super.prints();
+
         System.out.println("This is Child Class");
+  
     }
 
-    public static void main(String[] args) {
-        
-        Parent p = new Parent();
-        Child c = new Child();
-
-        p.prints();
-        c.prints();
-        c.prints();
-    }
+    
 }
