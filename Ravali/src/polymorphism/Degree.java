df2f9723ccc9414321ae1a5f8049225c9e3489f4
desaniravalireddy@gmail.com <desaniravalireddy@gmail.com>
2021-09-16 20:48:44 +0530

    method 5

---
+++ Ravali/src/polymorphism/Degree.java
@@ -0,0 +1,27 @@
+package polymorphism;
+
+public class Degree {
+    void getdegree(){
+        System.out.println("I got a degree");
+    }
+   class Ug{
+       void getdegree(){
+           System.out.println("I am a undergraduate");
+       }
+    }
+class Pg{
+    void getdegree(){
+        System.out.println("I am a postgraduate");
+    }
+}
+    
+   public static void main(String[]args){
+       Degree ob = new Degree();
+       ob.getdegree();
+       Degree.Ug ob1 = ob.new Ug();
+       ob1.getdegree();
+      Degree.Pg ob2 = ob.new Pg();
+      ob2.getdegree();
+
+   }
+}
