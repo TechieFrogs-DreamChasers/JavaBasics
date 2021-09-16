d654c14553dfb61c648218567b0f26485a6fd9ee
Ganesh T <ganeshkrishna.tu@gmail.com>
2021-09-16 11:31:26 +1000

    commit

--- Ganesh/src/Assignment3/Rectangle1.java
+++ Ganesh/src/Assignment3/Rectangle1.java
@@ -2,17 +2,17 @@ package Assignment3;
 
 public class Rectangle1 extends Shape {
     void displayRectangle(){
-        System.out.println("This is Rectangle");
+        System.out.println("This is Rectangle Shape");
     }
-    public class Square extends Rectangle1 {
-        void displaySquare(){
-            System.out.println("Square is Rectangle");
-        
-        } 
-   }
+}
+class Square2 extends Rectangle1 {
+    void displaySquare(){
+        System.out.println("Rectangle is Square");
+  }
+
 public static void main(String[] args){
-    Square s1 = new Square();
-    s1.displayRectangle();
+    Square2 s1 = new Square2();
+    s1.displaySquare();
     s1.displayShape();
-   }
+ }
 }
