881c3d3efc713a42aba4bd2648d16ebab5caed66
Nandini-2986 <nandu.garige@gmail.com>
2021-09-16 19:33:21 -0500

    commit

---
+++ Nandini/src/Assignment4/Exercise2/IntegerandChar.java
@@ -0,0 +1,16 @@
+package Assignment4.Exercise2;
+
+public class IntegerandChar {
+
+    
+
+    void show(int i,char s){
+        System.out.println("Print Integer and Character: "+ i + "  " + s);
+    }
+
+    void show(char s,int i){
+        System.out.println(" Print Character and Integer: "+ s+"  "+i);
+
+    }
+    
+}
---
+++ Nandini/src/Assignment4/Exercise2/Main.java
@@ -0,0 +1,10 @@
+package Assignment4.Exercise2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        IntegerandChar value = new IntegerandChar();
+        value.show(1,'e');
+        value.show(3,'h');
+    }
+}
