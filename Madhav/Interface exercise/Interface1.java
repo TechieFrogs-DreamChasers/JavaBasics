1ed76adf9a7dcd905b681f80c779445654e2458f
madhavkatta3333 <madhavkatta3333@gmail.com>
2021-09-20 02:48:54 +0530

    commit interface

--- Madhav/Interface exercise/Interface1.java
+++ Madhav/Interface exercise/Interface1.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 interface RegularPolygon{
     int getNumSides();
     int getSideLength();
+    
+    static int totalside()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of polygons");
+        int n = sc.nextInt();
+        int RegularPolygon[] = new int[n];
+        for(int i=0 ; i < n; i++)
+        {
+            RegularPolygon[i] = sc.nextInt();
+        }
+        int total = 0;
+        for(int j = 0 ; j<n ; j++)
+        {
+            total += RegularPolygon[j];
+        }
+        return total;
+    }
+    default int getPerimeter(int s,int l)
+    {
+        return s*l;
+    }
+    
+
     
 }
 class EquiTriangle implements RegularPolygon{
@@ -17,10 +43,7 @@ class EquiTriangle implements RegularPolygon{
     {
         this.i=s;
     }
-    public void getPerimeter()
-    {
-        System.out.println("Perimeter is "+(i*getNumSides()));
-    }
+   
     
 }
 class Square implements RegularPolygon{
@@ -39,20 +62,22 @@ class Square implements RegularPolygon{
     {
         this.s=s;
     }
-    public void getPerimeter()
-    {
-        System.out.println("Perimeter is "+(s*getNumSides()));
-    }
+    
      
 }
 public class Interface1{
     public static void main(String[] args) {
+     
+       System.out.println("total sides are"+RegularPolygon.totalside());
         Square sq = new Square(6);
+        
         System.out.println("No.of sides in a square is "+sq.getNumSides());
-        sq.getPerimeter();
+        System.out.print("The perimeter of square is ");
+        System.out.println(sq.getPerimeter(sq.getNumSides(),sq.getSideLength()));
         EquiTriangle eq = new EquiTriangle(4);
         System.out.println("No.of sides in a square is "+eq.getNumSides());
-        eq.getPerimeter();
+        System.out.print("The perimeter of Equilateral triangle is ");
+        System.out.println(eq.getPerimeter(eq.getNumSides(),eq.getSideLength()));
     }
     
 }
