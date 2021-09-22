a2aee6cc688fa7b4383298f3628608fe9f29abad
siri1592 <devisri1592@gmail.com>
2021-09-22 19:34:34 +0530

    Interfaces Assignment updated 2 files added

--- Devisri/Assignment-6/src/com/Programs/program7/Sub2/Model.java
+++ Devisri/Assignment-6/src/com/Programs/program7/Sub2/Model.java
@@ -1,11 +1,10 @@
 package com.Programs.program7.Sub2;
 
 import com.Programs.program7.Sub.IModel;
-import com.Programs.program7.Sub3.ShowSystemMode;
 
 public class Model {
 
-    protected class Mode implements IModel{
+    public class Mode implements IModel{
 
         @Override
         public void work() {
@@ -18,14 +17,4 @@ public class Model {
         }
     }
 
-   public void hibernate(Mode m){
-        m.rest();
-        m.work();
-    }
-    public static void main(String[] args) {
-        Model model=new Model();
-        Model.Mode mode=model.new Mode();
-        ShowSystemMode ssm=new ShowSystemMode();
-        ssm.hibernate(mode);
-    }
 }
--- Devisri/Assignment-6/src/com/Programs/program7/Sub3/ShowSystemMode.java
+++ Devisri/Assignment-6/src/com/Programs/program7/Sub3/ShowSystemMode.java
@@ -1,19 +1,19 @@
 package com.Programs.program7.Sub3;
 
+import com.Programs.program7.Sub.IModel;
 import com.Programs.program7.Sub2.Model;
 
 public class ShowSystemMode extends Model{
 
-    public static void main(String[] args) {
-        // if inner class is protected its showing error.
-        //make Mode access to public. it's allowing
-        Model model=new ShowSystemMode();
-       // Model.Mode mode=model.new Mode();
-        ShowSystemMode ssm=new ShowSystemMode();
-        //ssm.hibernate(mode);
-        //Model model1=new ShowSystemMode();
+    IModel hibernate(){
+       Model model=new Model();
+       Mode mode = model.new Mode();
+       return mode;
+    }
 
-       // model1.hibernate(mode);
+    public static void main(String[] args) {
+        ShowSystemMode ssm=new ShowSystemMode();
+        System.out.println(ssm.hibernate());
     }
 
 }
